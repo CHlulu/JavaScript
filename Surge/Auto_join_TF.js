7776d7ca77e16741a52ed32c890f346ea1bb2525
chlulu <1023436099@qq.com>
2025-06-21 08:33:13 +0800

    Update Auto_join_TF.js

--- Surge/Auto_join_TF.js
+++ Surge/Auto_join_TF.js
@@ -1,5 +1,5 @@
 !(async () => {
-  ids = $persistentStore.read('APP_ID')
+  ids = 'dDtSst4s6'
   if (ids == '') {
     $notification.post('所有TF已加入完毕','模块已自动关闭','')
     $done($httpAPI('POST', '/v1/modules', {'Auto module for JavaScripts': 'false'}))
@@ -13,14 +13,14 @@
   })();
   
   function autoPost(ID) {
-    let Key = $persistentStore.read('key')
+    let Key = '47b7aa8c-ca48-4518-84a0-1b1258423dab'
     let testurl = 'https://testflight.apple.com/v3/accounts/' + Key + '/ru/'
     let header = {
-      'X-Session-Id': `${$persistentStore.read('session_id')}`,
-      'X-Session-Digest': `${$persistentStore.read('session_digest')}`,
-      'X-Request-Id': `${$persistentStore.read('request_id')}`,
-      'X-Apple-AMD-X': `${$persistentStore.read('X-Apple-AMD-X')}`,
-      'User-Agent': `${$persistentStore.read('TFUA')}`
+      'X-Session-Id': 'CIquDBIQRkIc6PLzTWGlGbaGKu32cQ==',
+      'X-Session-Digest': '004bb8f77de61fe2138e237094aa9e15154eba2e',
+      'X-Request-Id': '5D1C1341-D830-4EC9-BEA7-2DF74FE055EF',
+      'X-Apple-AMD-M': '5rjEab5NxY8WACkJ52kq151lmWz/RMRccfKNlmianH4wBBhwaY/pRGyRYxKXKj1lQjVtnmHa7OWoOFN9',
+      'User-Agent': 'Oasis/3.9.0 OasisBuild/514.2 iOS/16.4.1 model/iPhone15,2 hwp/t8120 build/20E252 (6; dt:282) AMS/1 TSE/0'
     }
     return new Promise(function(resolve) {
       $httpClient.get({url: testurl + ID,headers: header}, function(error, resp, data) {
